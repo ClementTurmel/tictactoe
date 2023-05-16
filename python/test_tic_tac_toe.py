116a8be5d24f47cafd76be3b6e689e6d9f27b6e3
Clement TURMEL <clemturmel@gmail.com>
2023-05-16 16:26:56 +0200

    three cross on first lign Win ! and make some method 'private'

--- python/test_tic_tac_toe.py
+++ python/test_tic_tac_toe.py
@@ -1,5 +1,5 @@
 import pytest
-from tic_tac_toe import TicTacToeGame, Cell
+from tic_tac_toe import TicTacToeGame, TicTacToeWinner, Cell
 
 #https://codingdojo.org/kata/tic-tac-toe/
 # +---+---+---+
@@ -51,7 +51,6 @@ def test_tic_tac_toe_game_player_can_play_at_cell_5():
 
 
 def test_when_play_is_called_to_time_we_have_a_cross_and_a_round():
-
     expected = [
         [_,_,_], # 1 2 3 
         [_,_,_], # 4 5 6 
@@ -62,3 +61,21 @@ def test_when_play_is_called_to_time_we_have_a_cross_and_a_round():
         .play(7)\
         .play(9)\
         .get_grid() == expected
+    
+
+def test_when_first_lign_if_full_of_cross_player_cross_win():
+    expected = [
+        [X,X,X], # 1 2 3 
+        [O,O,_], # 4 5 6 
+        [_,_,_]  # 7 8 9 
+    ]
+
+    game = TicTacToeGame()\
+        .play(1)\
+        .play(4)\
+        .play(2)\
+        .play(5)\
+        .play(3)
+    
+    assert game.get_winner() == X
+    assert game.get_grid()   == expected
--- python/tic_tac_toe.py
+++ python/tic_tac_toe.py
@@ -19,6 +19,17 @@ cell_to_x_y =    {
 }
 
 
+class TicTacToeWinner:
+    def __init__(self, TicTacToeGame) -> None:
+        self.grid = TicTacToeGame.grid
+        self.winner = TicTacToeGame.player
+    
+    def get_grid(self):
+        return self.grid
+    
+    def get_winner(self):
+        return self.winner
+
 class TicTacToeGame:
 
     def __init__(self) -> None:
@@ -38,13 +49,27 @@ class TicTacToeGame:
         x, y = cell_to_x_y[cell_number]
         self.grid[x][y] = self.player
 
-        self.move_to_next_player()
+        have_a_winner = self.__check_grid()
 
-        return self
+        if have_a_winner:
+            return TicTacToeWinner(self)
+        else:
+            self.__move_to_next_player()
+            return self
     
-    def move_to_next_player(self):
+    def __move_to_next_player(self):
         if self.player == Cell.CROSS:
             self.player = Cell.ROUND
         else:
             self.player = Cell.CROSS
-    
+
+    def __check_grid(self):
+        if self.__get_cell(1) == self.__get_cell(2) == self.__get_cell(3) and self.__get_cell(1) is not Cell.EMPTY:
+            return self.__get_cell(1)
+        else:
+            return None
+        
+    def __get_cell(self, cell_number:int):
+        x, y = cell_to_x_y[cell_number]
+        return self.grid[x][y]
+
