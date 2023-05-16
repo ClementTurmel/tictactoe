4ed95158d9891aa4691bb1cfe4906ccf018daa33
Clement TURMEL <clemturmel@gmail.com>
2023-05-16 14:53:38 +0200

    Red test introducing second player

--- python/test_tic_tac_toe.py
+++ python/test_tic_tac_toe.py
@@ -48,3 +48,14 @@ def test_tic_tac_toe_game_player_can_play_at_cell_5():
     ]
 
     assert TicTacToeGame().play(5).get_grid() == expected
+
+
+def test_when_play_is_called_to_time_we_have_a_cross_and_a_round():
+
+    expected = [
+            [_,_,_], # 1 2 3 
+            [_,_,_], # 4 5 6 
+            [X,_,O]  # 7 8 9 
+    ]
+
+    assert TicTacToeGame().play(7).play(9).get_grid() == expected
